357404d571ff40b7b5d9513c82228ad20e4803f3
leec16 <leec16@192.168.0.111>
2018-12-09 20:22:40 -0500

    Just organize the chat gui page

--- src/ChatRoomController.java
+++ src/ChatRoomController.java
@@ -12,15 +12,6 @@ import javafx.scene.control.TextArea;
  * Date:11/30/2018
  */
 public class ChatRoomController {
-	 @FXML
-	    private TextArea tfEncryptedMessage;
-
-	    @FXML
-	    private TextArea tfDecryptedMessage;
-
-	    @FXML
-	    private Button btnDecrypt;
-
 	    @FXML
 	    private TextArea taChatRoom;
 
@@ -33,26 +24,25 @@ public class ChatRoomController {
 	    @FXML
 	    private Button btnSend;
 	
-	    @FXML
-	    void DecryptMessage(ActionEvent event) {
-
-	    }
-
-	    @FXML
-	    void EncryptMessage(ActionEvent event) {
-
-	    }
+	    //When you press enter butoon:
 	    public void clickStartChatroom(ActionEvent event) throws IOException {
+	    	// send the user message to sever...
+	    	String usermessage = taMessage.getText();
+	    	Client.getSendMessage(usermessage);
+	    	// I don't understand how you get the message from clienthandler...
+	    	taChatRoom.setText("?");
+	    	
+	    	
 	    }
 	    @FXML // This method is called by the FXMLLoader when initialization is complete
 	    void initialize() {
-	        assert tfEncryptedMessage != null : "fx:id=\"tfEncryptedMessage\" was not injected: check your FXML file 'ChatRoomView.fxml'.";
-	        assert tfDecryptedMessage != null : "fx:id=\"tfDecryptedMessage\" was not injected: check your FXML file 'ChatRoomView.fxml'.";
-	        assert btnDecrypt != null : "fx:id=\"btnDecrypt\" was not injected: check your FXML file 'ChatRoomView.fxml'.";
 	        assert taChatRoom != null : "fx:id=\"taChatRoom\" was not injected: check your FXML file 'ChatRoomView.fxml'.";
 	        assert taMessage != null : "fx:id=\"taMessage\" was not injected: check your FXML file 'ChatRoomView.fxml'.";
 	        assert taUserlist != null : "fx:id=\"taUserlist\" was not injected: check your FXML file 'ChatRoomView.fxml'.";
 	        assert btnSend != null : "fx:id=\"btnSend\" was not injected: check your FXML file 'ChatRoomView.fxml'.";
 
 	    }
+	    // and we need a method to keep update the message
+	    //extra : we can cancel the userlist.... but if we have time ,we can also update the userlist...
+	    
 }
